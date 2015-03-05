6ba4ba4f0d84ae4576c4597656ae42227c43247e
aric <aric.allen2@gmail.com>
2015-03-04 17:09:48 -0800

    Pushing back to github

--- word-grabber.php
+++ word-grabber.php
@@ -1,14 +1,5 @@
 <?php
 
-require '../instapush/vendor/autoload.php';
-$time = getdate();
-$time = "{$time['hours']}:$time{['minutes']}:{$time['seconds']}";
-$ip = new instaPush();
-$ip->App("52e51109a4c48af7623e370d", "17b6af22be9684868cceaf31a681425e");
-$ip->trackers("time", $time);
-$ip->Event("new game");
-$ip->Push();
-
 // reading uses URL paths
 $root_domain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
 
